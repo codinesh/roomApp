38c281203a51a6e8c4cd82ec339152f1a48fb449
Dinesh Kumar Reddy C O <codinesh@live.com>
2015-08-29 22:02:20 +0530

    Initial Version

---
+++ ngRoom/scripts/app.js
@@ -0,0 +1,22 @@
+﻿var app;
+(function () {
+    app = angular.module("roomApp", ['ngRoute']);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/addPerson', {
+                templateUrl: 'templates/addPerson.html',
+                controller: 'personController'
+            }).
+            when('/person', {
+                templateUrl: 'templates/addPerson.html',
+                controller: 'personController'
+            }).
+
+            otherwise('', {
+                redirectTo: 'home.html',
+            });
+
+        //$locationProvider.html5Mode(true);
+    }]);
+}());
---
+++ ngRoom/scripts/controllers/roommatesController.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="D:\Learning\TFS\GIT\LearningThings\ngRoom\ngRoom\lib/angular.js" />
+(function () {
+    var app = angular.module("roomApp");
+    var persons = [{ name: "Dinesh" }, { name: "Naveen" }, { name: "Adithya" }];
+    var curPerson = {};
+    app.controller("personController", ["$scope", "$filter", function ($scope, $filter) {
+        var sc = $scope;
+        sc.persons = persons;
+        sc.temp = {};
+        sc.curPerson = curPerson;
+        sc.addPerson = function (name) {
+            if (existsInArray(name)) {
+                persons.push({ name: name });
+                sc.temp.PersonName = null;
+            }
+            else {
+
+            }
+        }
+
+        var existsInArray = function (value) {
+            var found = $filter('filter')($scope.persons, { name: value }, false);
+            if (found.length) {
+                return false;
+            }
+            else
+                return true;
+        }
+    }]).
+    controller("homeController", ["$scope", "$filter", function ($scope, $filter) {
+        var sc = $scope;
+        sc.persons = persons;
+
+        var markCurrent = function (item) {
+            curPerson = item;
+        };
+    }]);
+}());
